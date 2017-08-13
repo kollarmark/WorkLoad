a8369826bad22933d842cbe18812c3a2fcacc2bb
Mark Kollar <kollarmark98@gmail.com>
2017-08-13 14:23:37 +0200

    Eintragsübersicht hinzugefügt.

--- WorkLoad_V1/src/workload/menu/EintragsUebersicht.java
+++ WorkLoad_V1/src/workload/menu/EintragsUebersicht.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,6 +14,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 
+import workload.eintrag.Eintrag;
 import workload.eintrag.EintragsVerwaltung;
 import workload.planung.PlanungsVerwaltung;
 
@@ -47,6 +50,25 @@ public class EintragsUebersicht extends JDialog {
 			
 			String[][] data = new String[EintragsVerwaltung.getInstance().getEintraege().size()][3];
 			String[] headers = { "Datum" , "Modul", "Betrag" };
+			
+			ArrayList<Eintrag> eint = EintragsVerwaltung.getInstance().getEintraege();
+			Eintrag[] temp = new Eintrag[eint.size()];
+			
+			for (int i = 0; i < temp.length; i++) {
+				temp[i] = eint.get(i);
+			}
+			
+			int monat = 0;
+			int tag = 0;
+			int concat = 0;
+			for (int i = 0; i < temp.length; i++) {
+				monat = temp[i].getKal().get(Calendar.MONTH)+1;
+				tag = temp[i].getKal().get(Calendar.DAY_OF_MONTH);
+				
+				concat = Integer.valueOf(String.valueOf(monat) + String.valueOf(tag)); // Für den Vergleich nach Datum zusammensetzung von Monat und Tag z.B. 12. Aug = 812
+				
+				System.out.println(concat);
+			}
 			
 			for (int i = 0; i < EintragsVerwaltung.getInstance().getEintraege().size(); i++) {
 				data[i][0] = EintragsVerwaltung.getInstance().getEintraege().get(i).getKal().getTime().toString();
